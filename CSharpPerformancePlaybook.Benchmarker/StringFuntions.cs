b07fc7f83d5624bb71c20d93d4a8386bfea47196
Josvany_G <josvanyg@gmail.com>
2022-08-18 17:30:10 -0600

    test

--- CSharpPerformancePlaybook.Benchmarker/StringFuntions.cs
+++ CSharpPerformancePlaybook.Benchmarker/StringFuntions.cs
@@ -5,34 +5,34 @@ namespace CSharpPerformancePlaybook.Benchmarker
 {
     public class StringFuntions
     {
-        //[Benchmark()]
-        //public void BuildStringBadly()
-        //{
-        //    var x = new StringWorker().BuildStringBadly("test");
-        //}
+        [Benchmark()]
+        public void BuildStringBadly()
+        {
+            var x = new StringWorker().BuildStringBadly("test");
+        }
 
-        //[Benchmark()]
-        //public void BuildStringBetter()
-        //{
-        //    var x = new StringWorker().BuildStringBetter("test");
-        //}
+        [Benchmark()]
+        public void BuildStringBetter()
+        {
+            var x = new StringWorker().BuildStringBetter("test");
+        }
 
-        //[Benchmark()]
-        //public void NativeSplit()
-        //{
-        //    var x = new StringWorker().NaiveSplitName("Josvany, García");
-        //}
+        [Benchmark()]
+        public void NativeSplit()
+        {
+            var x = new StringWorker().NaiveSplitName("Josvany, García");
+        }
 
-        //[Benchmark()]
-        //public void SplitSplit()
-        //{
-        //    var x = new StringWorker().SplitSplitName("Josvany, García");
-        //}
+        [Benchmark()]
+        public void SplitSplit()
+        {
+            var x = new StringWorker().SplitSplitName("Josvany, García");
+        }
 
-        //[Benchmark()]
-        //public void SpanSplit()
-        //{
-        //    var x = new StringWorker().SpanSplitName("Josvany, García");
-        //}
+        [Benchmark()]
+        public void SpanSplit()
+        {
+            var x = new StringWorker().SpanSplitName("Josvany, García");
+        }
     }
 }
